dd5e33587e0046af7ac1909cefb982c4dccae0d3
NGUYEN THE LAM <tapkichbug360@gmail.com>
2026-03-06 13:51:26 +0700

    Fix: run bot.py instead of main.py

--- start.sh
+++ start.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
-# Chạy Flask ở foreground (quan trọng: không có --daemon)
 gunicorn main:app &
-# Lưu PID của gunicorn
-GUNICORN_PID=$!
-# Chạy bot ở foreground
 python bot.py
-# Khi bot kết thúc, kết thúc cả gunicorn
-kill $GUNICORN_PID
